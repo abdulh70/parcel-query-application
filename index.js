ae879a4688adfce20631d91d5332f198517ec6c9
abdulh70 <abdulazizh70@gmail.com>
2024-10-17 16:12:47 +0300

    changed link

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Fetch parcels from the JSON server and display them in the table
 function fetchParcels() {
-    fetch('http://localhost:3000/parcels')
+    fetch('https://my-json-server.typicode.com/abdulh70/parcel-query-application/parcels')
         .then(response => response.json())
         .then(data => displayParcels(data))
         .catch(error => console.error('Error fetching parcels:', error));
@@ -31,7 +31,7 @@ function displayParcels(parcels) {
 // Search parcels based on input criteria
 function searchParcels() {
     const query = document.getElementById('search-input').value.toLowerCase();
-    fetch('http://localhost:3000/parcels')
+    fetch('https://my-json-server.typicode.com/abdulh70/parcel-query-application/parcels')
         .then(response => response.json())
         .then(data => {
             const filteredParcels = data.filter(parcel => 
